61232038c9ebba0ec9bb81acde0f9e5e881319cb
damshxy <mrsaddam2806@gmail.com>
2023-11-29 12:17:35 +0700

    fixing: fix on questions

--- cmd/server/laravel_grpc/question/qustion_server.go
+++ cmd/server/laravel_grpc/question/qustion_server.go
@@ -7,11 +7,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 	pb_laravel_question "whatw-golang/pb/laravel/question"
 )
 
 type ServerQuestion struct {
-	pb_laravel_question.QuestionServiceServer
+	pb_laravel_question.UnimplementedQuestionServiceServer
 }
 
 var apiResponse struct {
@@ -29,27 +31,36 @@ var apiResponse struct {
 }
 func (*ServerQuestion) GetQuestion(ctx context.Context, req *pb_laravel_question.QuestionRequest) (*pb_laravel_question.QuestionResponse, error) {
 	apiURL := os.Getenv("API_URL_LARAVEL_QUESTION")
-	resp, err := http.Get(apiURL)
+	client := http.Client{
+		Timeout: 10 * time.Second, // Set a timeout for the HTTP request
+	}
+	resp, err := client.Get(apiURL)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch data from API: %v", err)
 	}
-	
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API request failed with status code: %d", resp.StatusCode)
+	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read API response body: %v", err)
 	}
 
+	// Check if the response is HTML
+	if strings.HasPrefix(string(data), "<") {
+		return nil, fmt.Errorf("API returned HTML error page: %s", string(data))
+	}
 
 	if err := json.Unmarshal(data, &apiResponse); err != nil {
 		return nil, fmt.Errorf("failed to unmarshal API response: %v", err)
 	}
-	
+
 	if !apiResponse.Success || len(apiResponse.Data) == 0 {
 		return nil, fmt.Errorf("API response indicates failure or empty data")
 	}
-	
 
 	var dataRespose []*pb_laravel_question.Question
 	for _, q := range apiResponse.Data {
